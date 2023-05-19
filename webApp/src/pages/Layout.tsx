e742bd01c2243a88f05deede71bd9a44d78c717c
CHA Léon <juneleon.cha@gmail.com>
2023-05-19 17:15:50 +0200

    add condition for mobile or desktop

--- webApp/src/pages/Layout.tsx
+++ webApp/src/pages/Layout.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode} from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
 
@@ -9,14 +9,41 @@ interface LayoutProps {
 	className?: string;
 }
 
-const Layout: React.FC<LayoutProps> = ({content, title, arrowBack, className}) => {
+const Layout: React.FC<LayoutProps> = ({ content, title, arrowBack, className }) => {
+
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsMobile(window.innerWidth < 700);
+		};
+		window.addEventListener('resize', handleResize);
+		handleResize();
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, [])
+
 	return (
 		<div id='content'>
-			<Header title={title} arrowBack={arrowBack}/>
-			<div className={`${className} layout`}>
-				{content}
-			</div>
-			<NavBar/>
+			{!isMobile ? (
+				<>
+					<Header title={title} arrowBack={arrowBack} />
+					<div className={`${className} layout`}>
+						{content}
+					</div>
+				</>
+			) : (
+				<>
+					<Header title={title} arrowBack={arrowBack} />
+					<div className={`${className} layout`}>
+						{content}
+					</div>
+					<NavBar />
+				</>
+			)}
+
 		</div>
 	);
 };
